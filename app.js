1f5c0544f6ade882e44bacd3a1cb91d032ed65d8
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2022-08-08 17:59:19 +0100

    form rules

--- app.js
+++ app.js
@@ -8,26 +8,26 @@
 const button = document.querySelector(".button");
 let inputFields = [...document.getElementsByTagName("input")];
 const allErrorElements = [...document.querySelectorAll(".error")];
+const cardName = document.querySelector(".front-card-name");
+const cardNumber = document.querySelector(".front-card-number");
+const cardMonth = document.querySelector(".front-card-month");
+const cardYear = document.querySelector(".front-card-year");
+const cardCvc = document.querySelector(".back-card-cvc");
 let errorFlag = false;
 
 /**checks the format of fields then submits them */
 const submitInfo = () => {
   resetErrorFlags();
   checkFormat();
-  checkIfEmpty();
+  checkEmpty();
   if (errorFlag) return;
 };
 
 /** checks if any of the fields are emptyand throxs an error glag */
-const checkIfEmpty = () => {
+const checkEmpty = () => {
   inputFields.forEach((input) => {
     if (input.value.length == 0) {
-      if (input.id == "month" || input.id == "year")
-        input = input.parentElement;
-      const errorElement = input.parentElement.lastElementChild;
-      errorElement.textContent = "Can't be blank";
-      errorElement.classList.add("show");
-      errorFlag = true;
+      showError(input, "Can't be blank");
     }
   });
 };
@@ -44,16 +44,89 @@ const resetErrorFlags = () => {
 const checkFormat = () => {
   for (let index = 1; index < inputFields.length; index++) {
     let input = inputFields[index];
-    let isNum = /^\d+$/.test(input.value);
+    let isNum = /^[\d ]*$/.test(input.value);
     if (!isNum) {
-      if (input.id == "month" || input.id == "year")
-        input = input.parentElement;
-      const errorElement = input.parentElement.lastElementChild;
-      errorElement.textContent = "Wrong format, numbers only";
-      errorElement.classList.add("show");
-      errorFlag = true;
+      showError(input, "Wrong format, numbers only");
     }
   }
 };
 
+/** shows error message */
+const showError = (input, errorMessage) => {
+  if (input.id == "month" || input.id == "year") input = input.parentElement;
+  const errorElement = input.parentElement.lastElementChild;
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("show");
+  errorFlag = true;
+};
+
+/** handles the format of the value of the fields */
+const formatHandler = (input, e) => {
+  // card name
+  if (input.id == "name") {
+    if (e.keyCode == 8) {
+      if (input.value.length == 0) cardName.textContent = "FELICIA LEIRE";
+      return;
+    }
+    cardName.textContent = input.value;
+  }
+
+  // card number
+  if (input.id == "cardnumber") {
+    if (e.keyCode == 8) {
+      if (input.value.length == 0)
+        cardNumber.textContent = "9591 6489 6389 101E";
+      return;
+    }
+    if (
+      input.value.length == 4 ||
+      input.value.length == 9 ||
+      input.value.length == 14
+    ) {
+      input.value += " ";
+    }
+    cardNumber.textContent = input.value;
+    if (input.value.length == 19) document.getElementById("month").focus();
+  }
+
+  // month
+  if (input.id == "month") {
+    if (e.keyCode == 8) {
+      if (input.value.length == 0) cardMonth.textContent = "09";
+      return;
+    }
+    let isFormated = /^(0[1-9])$|^(1[0-2]?)$|^(\d{1})$/.test(input.value);
+    if (isFormated) {
+      let value = input.value;
+      if (value.length == 1) value = `0${value}`;
+      cardMonth.textContent = value;
+    } else input.value = "";
+    if (input.value.length == 2) document.getElementById("year").focus();
+  }
+
+  // year
+  if (input.id == "year") {
+    if (e.keyCode == 8) {
+      if (input.value.length == 0) cardYear.textContent = "00";
+      return;
+    }
+    let value = input.value;
+    if (value.length == 1) value = `0${value}`;
+    cardYear.textContent = value;
+    if (input.value.length == 2) document.getElementById("cvc").focus();
+  }
+
+  // cvc
+  if (input.id == "cvc") {
+    if (e.keyCode == 8) {
+      if (input.value.length == 0) cardCvc.textContent = "000";
+      return;
+    }
+    cardCvc.textContent = input.value;
+  }
+};
+
 button.addEventListener("click", submitInfo);
+inputFields.forEach((input) => {
+  input.addEventListener("input", (e) => formatHandler(input, e));
+});
